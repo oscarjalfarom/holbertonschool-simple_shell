952bfdf9706a68893e9da28c56344d05f2575ff9
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-08-23 12:53:38 -0500

    Starting Simple Shell 1.0

--- main.c
+++ main.c
@@ -1,43 +1,50 @@
 #include "main.h"
 
-int main(void)
-{
-    char *user_input[1024];
-    char *token, *line = NULL, *full_path = NULL;
-    pid_t child_pid;
-    size_t i, line_buffer_size;
-    int status = 0;
+/**
+ * main - open shell, project base
+ * Return: int
+ */
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf(" $ ");
-        fflush(stdin);
-        signal(SIGINT, interruptHandler);
-        if (getline(&line, &line_buffer_size, stdin) == EOF)
-            break;     
-        if (*line == '\n' || *line == '\t')
-            continue;
-        token = strtok(line, " \t\n");
-        for (i = 0; i < 1024 && token != NULL; i++)
-            user_input[i] = token, token = strtok(NULL, " \t\n");
-        user_input[i] = NULL;
-        if (user_input[0] == NULL)
-            continue;
-        full_path = pathfinder(user_input[0]);
-        if (builtin(user_input[0]) == 0)
-        {
-            child_pid = fork();
-            if (child_pid == 0)
-            {
-                if (execve(full_path, user_input, environ))
-                {
-                    perror("execve"),free(line), exit(EXIT_FAILURE);
-                }
-            }   if (child_pid > 0)
-                    wait(&status), free(line);
-        }
-        line = NULL, token = NULL, user_input[0] = NULL;
-    }
-    free(line), free(full_path), exit(status); 
-}
+ int main(void)
+ {
+ 	char *buffer = NULL, **args;
+ 	size_t line_buffer_size = 0;
+ 	ssize_t buff_size = 0;
+ 	int status = 0;
+
+ 	while (1)
+ 	{
+ 		if (isatty(0))
+ 			printf("($) ");
+
+ 		buff_size = getline(&buffer, &line_buffer_size, stdin);
+ 		if (buff_size == -1 || _strcmp("exit\n", buffer) == 0)
+ 		{
+ 			free(buffer);
+ 			break;
+ 		}
+ 		buffer[buff_size - 1] = '\0';
+
+ 		if (_strcmp("env", buffer) == 0)
+ 		{
+ 			_env();
+ 			continue;
+ 		}
+
+ 		if (empty_line(buffer) == 1)
+ 		{
+ 			status = 0;
+ 			continue;
+ 		}
+
+ 		args = _split(buffer, "");
+ 		args[0] = search_path(args[0]);
+
+ 		if (args[0] != NULL)
+ 			status = execute(args);
+ 		else
+ 			perror("Error");
+ 		free(args);
+ 	}
+ 	return (status);
+ }
--- main.h
+++ main.h
@@ -1,22 +1,28 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
+/*LIBRARIES*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+/*PROTOTYPES*/
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *search_path(char *command);
+char **_split(char *str, char *sep);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_getenv(char *env_var);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void _env(void);
+int execute(char **args);
+int empty_line(char *buff);
+
+/* GLOBAL VAR*/
 extern char **environ;
-int builtin(char *user_input);
-void interruptHandler(int signal);
-char compare_strings(char *string1, char *string2, int max_length);
-int string_length(char *string);
-char *string_concat(char *string1, char *string2);
-int display_environment_var(void);
-char *_getEnvironmentValue(const char *name);
-char *pathfinder(char *user_imput);
 
 #endif
