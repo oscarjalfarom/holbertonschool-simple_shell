ab49817df83d0bb39c1abcaf8eacf1cce2860c90
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-08-24 19:11:39 -0500

    Simple, Shell 2.3

--- main.h
+++ main.h
@@ -1,16 +1,20 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdbool.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 
 extern char **environ;
 
+bool _isnumber(const char *str);
+void custom_exit(int status);
 void trim(char *str);
 void display_environment_var(void);
 char *pathfinder(char *command);
