5b5288648ab887fcc0c9f864c429a4c631d1ead0
oscarjalfaromo <dev.oscarjalfarom@gmail.com>
2023-08-25 22:00:50 -0500

    Simple, Shell 3.0

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+void trim(char *str);
+void display_environment_var(void);
+char *pathfinder(char *command);
+void interruptHandler(int signal);
+int builtin(char **comand);
+void execComand(char *full_path, char **comand);
+
+#endif
